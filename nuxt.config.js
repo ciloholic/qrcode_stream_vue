06bdf10e18b7d1acf128aa04b4195d830e9139ca
ciloholic <ciloholic0119@gmail.com>
2019-01-07 19:19:19 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'spa',
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  loading: { color: '#fff' },
+  css: ['element-ui/lib/theme-chalk/index.css'],
+  plugins: ['@/plugins/element-ui'],
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+  build: {
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}

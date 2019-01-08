418731022ef05d3feed677f1f43edadb57396f27
ciloholic <ciloholic0119@gmail.com>
2019-01-09 07:51:39 +0900

    Fix mode universal -> spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 const pkg = require('./package')
-
 const ghPagesOnly =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
@@ -10,7 +9,7 @@ const ghPagesOnly =
     : {}
 
 module.exports = {
-  mode: 'universal',
+  mode: 'spa',
   head: {
     title: pkg.name,
     meta: [
